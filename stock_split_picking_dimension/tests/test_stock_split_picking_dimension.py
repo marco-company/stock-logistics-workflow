91fef2b6ff1043ecfd770d930fc080d8b4891aa1
Iván Todorovich <ivan.todorovich@gmail.com>
2025-08-14 12:07:53 -0300

    [REF] stock_split_picking_dimension: split off moves in a new picking
    Implements the changes in #2062
    
    When a transfer is being partially processed by a user and you want to validate
    only that, you should create a new split order containing the done lines and
    keep remaining lines in the original transfer.
    
    In business terms, a split order represent an extraction of an original order
    that is being partially processed. A back order is the remaining of an order
    that was not entirely available.

--- stock_split_picking_dimension/tests/test_stock_split_picking_dimension.py
+++ stock_split_picking_dimension/tests/test_stock_split_picking_dimension.py
@@ -100,11 +100,8 @@ class TestStockSplitPickingDimension(TestStockSplitPickingCase):
             )
             new_picking = rc.records
 
-        self.assertEqual(len(self.picking.move_ids), 1)
-        self.assertEqual(self.picking.move_ids, self.move)
-
-        self.assertEqual(len(new_picking.move_ids), 2)
-        self.assertEqual(new_picking.move_ids, self.move_2 + self.move_3)
+        self.assertEqual(new_picking.move_ids, self.move)
+        self.assertEqual(self.picking.move_ids, self.move_2 + self.move_3)
 
     def test_wizard_split_volume(self):
         with RecordCapturer(self.env["stock.picking"], []) as rc:
@@ -117,13 +114,10 @@ class TestStockSplitPickingDimension(TestStockSplitPickingCase):
             )
             new_picking = rc.records
 
-        self.assertEqual(len(self.picking.move_ids), 2)
         # The second move is by passed because it's volume is 2000
         # but the sum of the first and third move is 2500 which is less than 2600
-        self.assertEqual(self.picking.move_ids, self.move + self.move_3)
-
-        self.assertEqual(len(new_picking.move_ids), 1)
-        self.assertEqual(new_picking.move_ids, self.move_2)
+        self.assertEqual(new_picking.move_ids, self.move + self.move_3)
+        self.assertEqual(self.picking.move_ids, self.move_2)
 
     def test_wizard_split_weight(self):
         with RecordCapturer(self.env["stock.picking"], []) as rc:
@@ -136,10 +130,69 @@ class TestStockSplitPickingDimension(TestStockSplitPickingCase):
             )
             new_picking = rc.records
 
-        self.assertEqual(len(self.picking.move_ids), 2)
         # The second move is by passed because it's weight is 20
         # but the sum of the first and second move is 30 which is more than 25
+        self.assertEqual(new_picking.move_ids, self.move + self.move_3)
+        self.assertEqual(self.picking.move_ids, self.move_2)
+
+    def test_wizard_ignore_cancelled_moves(self):
+        self.move._action_cancel()
+
+        with RecordCapturer(self.env["stock.picking"], []) as rc:
+            self._split_picking(
+                self.picking,
+                mode="dimensions",
+                max_nbr_lines=1,
+                max_volume=0,
+                max_weight=0,
+            )
+            new_picking = rc.records
+
+        self.assertEqual(new_picking.move_ids, self.move_2)
         self.assertEqual(self.picking.move_ids, self.move + self.move_3)
 
-        self.assertEqual(len(new_picking.move_ids), 1)
-        self.assertEqual(new_picking.move_ids, self.move_2)
+    def test_wizard_user_volume_uom(self):
+        # The default system volume is cubic meter, but we change it
+        # to litre to test the conversion
+        wizard = (
+            self.env["stock.split.picking"]
+            .with_context(active_ids=self.picking.ids)
+            .create(
+                {
+                    "mode": "dimensions",
+                    "max_nbr_lines": 0,
+                    "max_volume": 1,
+                    "max_weight": 0,
+                    "user_volume_uom_id": self.env.ref("uom.product_uom_litre").id,
+                }
+            )
+        )
+        # The max_volume is expressed in the system volume (1 m3)
+        # However, the user_max_volume is expressed in the user volume (1000L)
+        self.assertAlmostEqual(wizard.user_max_volume, 1000)
+        # Setting the user_max_volume should update the max_volume
+        wizard.user_max_volume = 2000
+        self.assertAlmostEqual(wizard.max_volume, 2)
+
+    def test_wizard_user_weight_uom(self):
+        # The default system weight is kg, but we change it
+        # to gram to test the conversion
+        wizard = (
+            self.env["stock.split.picking"]
+            .with_context(active_ids=self.picking.ids)
+            .create(
+                {
+                    "mode": "dimensions",
+                    "max_nbr_lines": 0,
+                    "max_volume": 0,
+                    "max_weight": 1,
+                    "user_weight_uom_id": self.env.ref("uom.product_uom_gram").id,
+                }
+            )
+        )
+        # The max_weight is expressed in the system weight (1 kg)
+        # However, the user_max_weight is expressed in the user weight (1000g)
+        self.assertAlmostEqual(wizard.user_max_weight, 1000)
+        # Setting the user_max_weight should update the max_weight
+        wizard.user_max_weight = 2000
+        self.assertAlmostEqual(wizard.max_weight, 2)
